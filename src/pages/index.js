c5d3bfb2ac431c6496818a06159133536d18c39b
Adam N <anoah1225@gmail.com>
2020-01-05 21:16:05 -0500

    testimonials

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout"
-import Testimonials from "../components/Testimonials"
+import Testimonials from "../components/testimonials"
 import UserForm from '../components/UserForm'
 
 import '../styles/styles.scss'
