099cb38efea182e0e15dcaf9900f1ba04ea8d7b6
Adam N <anoah1225@gmail.com>
2020-01-08 20:47:50 -0500

    form revision

--- src/components/FormUserDetails.js
+++ src/components/FormUserDetails.js
@@ -3,6 +3,8 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 import formStyles from './formUser.module.scss'
+import Zipo from './zipo'
+import Zipd from './zipd'
 
 export class FormUserDetails extends Component {
     continue = e => {
@@ -14,8 +16,12 @@ export class FormUserDetails extends Component {
     render() {
         const { values, handleChange } = this.props;
         return (
+
+            
             <MuiThemeProvider>
+
                 <div className={formStyles.formText}>
+
                 <React.Fragment>
             <h3>Getting a Quote is Free & Easy</h3>
             <h4>We Care About Your Shipment</h4>
@@ -24,64 +30,79 @@ export class FormUserDetails extends Component {
             <h5>- Supreme Costumer Service</h5><br />
             <p><strong>Fill out the form below for a shipping quote</strong></p>
                 <div className={formStyles.fields}>
-                    
+                <Zipo
+                onChange={handleChange('zipo')}
+                defaultValue={values.zipo}
+                 />
+                 <Zipd
+                onChange={handleChange('zipd')}
+                defaultValue={values.zipd}
+                 />
+                 
+                    {/*                     
                     <TextField
                         hintStyle={{ color: 'white' }}
                         hintText="Enter Zip Code"
                         floatingLabelText="Where Are You Shipping From"
                         onChange={handleChange('zipo')}
                         defaultValue={values.zipo}
-                    />
-                    <br/>
+                    /> */}
+                    {/* <br/>
                     <TextField
                         hintStyle={{ color: 'white' }}
                         hintText="Enter Zip Code"
                         floatingLabelText="Where Are You Shipping To"
                         onChange={handleChange('zipd')}
                         defaultValue={values.zipd}
-                    />
-                    <br/>
-                     <TextField
-                        hintStyle={{ color: 'white' }}
-                        hintText="DD/MM/YYYY"
-                        floatingLabelText="Pick Up Date"
-                        onChange={handleChange('date')}
-                        defaultValue={values.date}
-                    />
+                    /> */}
+
+                    
                     </div>
-                    <br />
-                    <div className={formStyles.fields}>
+                  
+                    <div className={formStyles.fields2}>
                     <TextField
-                        hintStyle={{ color: 'white' }}
+                        hintStyle={{color:'white'}}
                         hintText="e.g. 2010"
                         floatingLabelText="Year"
                         onChange={handleChange('year')}
                         defaultValue={values.year}
                     />
-                    <br/>
+                    
                     <TextField
-                        hintStyle={{ color: 'white' }}
+                        hintStyle={{color:'white'}}
+                        label="e.g. Nissan"
                         hintText="e.g. Nissan"
                         floatingLabelText="Make"
                         onChange={handleChange('make')}
                         defaultValue={values.make}
                     />
-                    <br/>
+                   
                      <TextField
-                        hintStyle={{ color: 'white' }}
+                        hintStyle={{color:'white'}}
+                        label="e.g. Maxima"
                         hintText="e.g. Maxima"
                         floatingLabelText="Model"
                         onChange={handleChange('model')}
                         defaultValue={values.model}
+                    />                 
+                  
+                    <TextField
+                        hintStyle={{color:'white'}}
+                        label="Pick Up Date"
+                        variant="standard"
+                        hintText="DD/MM/YYYY"
+                        floatingLabelText="Pick Up Date"
+                        onChange={handleChange('date')}
+                        defaultValue={values.date}
                     />
-                    </div>
-                    <br />
+                    
                     <RaisedButton 
                         label="Continue"
                         primary={true}
                         style={styles.button}
                         onClick={this.continue}
                     />
+                    </div>
                 </React.Fragment>
                 </div>
             </MuiThemeProvider>
@@ -92,10 +113,110 @@ export class FormUserDetails extends Component {
 const styles = {
     button: {
         margin: 15
-    },
-    font: {
-        fontColor: "white"
     }
 }
+
+const top100Films = [
+    { title: 'The Shawshank Redemption', year: 1994 },
+    { title: 'The Godfather', year: 1972 },
+    { title: 'The Godfather: Part II', year: 1974 },
+    { title: 'The Dark Knight', year: 2008 },
+    { title: '12 Angry Men', year: 1957 },
+    { title: "Schindler's List", year: 1993 },
+    { title: 'Pulp Fiction', year: 1994 },
+    { title: 'The Lord of the Rings: The Return of the King', year: 2003 },
+    { title: 'The Good, the Bad and the Ugly', year: 1966 },
+    { title: 'Fight Club', year: 1999 },
+    { title: 'The Lord of the Rings: The Fellowship of the Ring', year: 2001 },
+    { title: 'Star Wars: Episode V - The Empire Strikes Back', year: 1980 },
+    { title: 'Forrest Gump', year: 1994 },
+    { title: 'Inception', year: 2010 },
+    { title: 'The Lord of the Rings: The Two Towers', year: 2002 },
+    { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
+    { title: 'Goodfellas', year: 1990 },
+    { title: 'The Matrix', year: 1999 },
+    { title: 'Seven Samurai', year: 1954 },
+    { title: 'Star Wars: Episode IV - A New Hope', year: 1977 },
+    { title: 'City of God', year: 2002 },
+    { title: 'Se7en', year: 1995 },
+    { title: 'The Silence of the Lambs', year: 1991 },
+    { title: "It's a Wonderful Life", year: 1946 },
+    { title: 'Life Is Beautiful', year: 1997 },
+    { title: 'The Usual Suspects', year: 1995 },
+    { title: 'Léon: The Professional', year: 1994 },
+    { title: 'Spirited Away', year: 2001 },
+    { title: 'Saving Private Ryan', year: 1998 },
+    { title: 'Once Upon a Time in the West', year: 1968 },
+    { title: 'American History X', year: 1998 },
+    { title: 'Interstellar', year: 2014 },
+    { title: 'Casablanca', year: 1942 },
+    { title: 'City Lights', year: 1931 },
+    { title: 'Psycho', year: 1960 },
+    { title: 'The Green Mile', year: 1999 },
+    { title: 'The Intouchables', year: 2011 },
+    { title: 'Modern Times', year: 1936 },
+    { title: 'Raiders of the Lost Ark', year: 1981 },
+    { title: 'Rear Window', year: 1954 },
+    { title: 'The Pianist', year: 2002 },
+    { title: 'The Departed', year: 2006 },
+    { title: 'Terminator 2: Judgment Day', year: 1991 },
+    { title: 'Back to the Future', year: 1985 },
+    { title: 'Whiplash', year: 2014 },
+    { title: 'Gladiator', year: 2000 },
+    { title: 'Memento', year: 2000 },
+    { title: 'The Prestige', year: 2006 },
+    { title: 'The Lion King', year: 1994 },
+    { title: 'Apocalypse Now', year: 1979 },
+    { title: 'Alien', year: 1979 },
+    { title: 'Sunset Boulevard', year: 1950 },
+    { title: 'Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb', year: 1964 },
+    { title: 'The Great Dictator', year: 1940 },
+    { title: 'Cinema Paradiso', year: 1988 },
+    { title: 'The Lives of Others', year: 2006 },
+    { title: 'Grave of the Fireflies', year: 1988 },
+    { title: 'Paths of Glory', year: 1957 },
+    { title: 'Django Unchained', year: 2012 },
+    { title: 'The Shining', year: 1980 },
+    { title: 'WALL·E', year: 2008 },
+    { title: 'American Beauty', year: 1999 },
+    { title: 'The Dark Knight Rises', year: 2012 },
+    { title: 'Princess Mononoke', year: 1997 },
+    { title: 'Aliens', year: 1986 },
+    { title: 'Oldboy', year: 2003 },
+    { title: 'Once Upon a Time in America', year: 1984 },
+    { title: 'Witness for the Prosecution', year: 1957 },
+    { title: 'Das Boot', year: 1981 },
+    { title: 'Citizen Kane', year: 1941 },
+    { title: 'North by Northwest', year: 1959 },
+    { title: 'Vertigo', year: 1958 },
+    { title: 'Star Wars: Episode VI - Return of the Jedi', year: 1983 },
+    { title: 'Reservoir Dogs', year: 1992 },
+    { title: 'Braveheart', year: 1995 },
+    { title: 'M', year: 1931 },
+    { title: 'Requiem for a Dream', year: 2000 },
+    { title: 'Amélie', year: 2001 },
+    { title: 'A Clockwork Orange', year: 1971 },
+    { title: 'Like Stars on Earth', year: 2007 },
+    { title: 'Taxi Driver', year: 1976 },
+    { title: 'Lawrence of Arabia', year: 1962 },
+    { title: 'Double Indemnity', year: 1944 },
+    { title: 'Eternal Sunshine of the Spotless Mind', year: 2004 },
+    { title: 'Amadeus', year: 1984 },
+    { title: 'To Kill a Mockingbird', year: 1962 },
+    { title: 'Toy Story 3', year: 2010 },
+    { title: 'Logan', year: 2017 },
+    { title: 'Full Metal Jacket', year: 1987 },
+    { title: 'Dangal', year: 2016 },
+    { title: 'The Sting', year: 1973 },
+    { title: '2001: A Space Odyssey', year: 1968 },
+    { title: "Singin' in the Rain", year: 1952 },
+    { title: 'Toy Story', year: 1995 },
+    { title: 'Bicycle Thieves', year: 1948 },
+    { title: 'The Kid', year: 1921 },
+    { title: 'Inglourious Basterds', year: 2009 },
+    { title: 'Snatch', year: 2000 },
+    { title: '3 Idiots', year: 2009 },
+    { title: 'Monty Python and the Holy Grail', year: 1975 },
+  ];
 
 export default FormUserDetails
