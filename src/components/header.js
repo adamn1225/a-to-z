7fb0650834221929c87aa2e75dd95e6f1e55aa83
Adam <Anoah1225@gmail.com>
2020-04-14 10:31:12 -0400

    mobile menu

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link, graphql, useStaticQuery } from 'gatsby'
 import { Button } from 'react-bootstrap';
 import headerStyles from './header.module.scss'
+import FaAlignJustify from 'react-icons/lib/fa/align-justify'
 
 
 const Header = () => {
@@ -37,17 +38,38 @@ const Header = () => {
                 <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}  to="/blog">Blog</Link>
                     </li><br />
                      <li> 
-                <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}  to="/about">About</Link>
-                    </li><br />
+                    <div className={headerStyles.dropdown}>
+                <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}  to="/about">Transport Services</Link>
+                      <div className={headerStyles.dropdownItems}>
+                       <a href="#">Link 1</a>
+                       <a href="#">Link 2</a>
+                       <a href="#">Link 3</a>
+                     </div>
+                    </div>
+                    </li>
+                    <br />
+                    <li>
+                <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}  to="/how">How it Works</Link>
+                    </li>
+
+
                      <li>
                 <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem}  to="/contact">Contact</Link>
                     </li>
-                
-                    {/* <li className={headerStyles.mcNum}>
-                    <p>MC# 1322334</p>
-                    <p className={headerStyles.vline}>Talk To Us!<br /> <a href="tel:800-888-8888">800-888-8888</a></p>
-                    </li> */}
+
                 </ul>
+
+                {/* mobile menu */}
+
+                <div className={headerStyles.mobList}>
+                    <h1><FaAlignJustify /></h1>
+                    <div className={headerStyles.mobItems}>
+                       <Link to="/blog">Blog</Link>
+                       <a href="#">Link 2</a>
+                       <a href="#">Link 3</a>
+                    </div>
+                </div>
+
 
               <div className={headerStyles.mcNum}>
               <p>
---
+++ src/pages/how.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const How = () => {
+    return (
+        <Layout>
+                        <h1>Contact me</h1>
+            <p>Need a developer? <a href="https://www.facebook.com/" target="_blank">Contact me.</a></p>
+        </Layout>
+
+    )
+}
+
+export default How;
